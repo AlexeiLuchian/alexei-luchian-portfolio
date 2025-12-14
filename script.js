c1e20ef378b4e51e70c61ff8b2f4648b1409be31
AlexeiLuchian <alexeiluchian999@gmail.com>
2025-12-14 16:50:24 +0200

    Add scroll animations in javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+const observerOptions = {
+    threshold: 0.2
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, observerOptions);
+
+const elementsToAnimate = document.querySelectorAll('.section-title, .skill-card, .project-card, contact-box');
+
+elementsToAnimate.forEach( el => {
+    el.classList.add('hidden');
+    observer.observe(el);
+})
